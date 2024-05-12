0c59a0a18822330ab53c142d079ac967e3e668f4
zergqw <eremenko.denis-qw@yandex.ru>
2024-05-12 17:32:15 +0300

    step3

--- gendiff.js
+++ gendiff.js
@@ -4,6 +4,6 @@ const program = new Command();
 program
   .description('CLI to some JavaScript string utilities')
   .version('0.8.0')
-  .option('-f, --format [type]', 'output format');
-
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
 program.parse();
